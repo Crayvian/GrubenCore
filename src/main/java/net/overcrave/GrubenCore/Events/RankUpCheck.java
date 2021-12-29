a7bc5df0214c786e208e0fe6e25cd079cc710ab6
OverCrave <over.crave@rubygenix.com>
2021-12-29 23:07:53 +0100

    Update RankUpCheck.java

--- src/main/java/net/overcrave/GrubenCore/Events/RankUpCheck.java
+++ src/main/java/net/overcrave/GrubenCore/Events/RankUpCheck.java
@@ -2,6 +2,7 @@ package net.overcrave.GrubenCore.Events;
 
 import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.events.experience.McMMOPlayerLevelChangeEvent;
+import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
 import com.gmail.nossr50.util.player.UserManager;
 import net.overcrave.GrubenCore.Main;
 import org.bukkit.Bukkit;
@@ -13,9 +14,8 @@ import org.bukkit.event.Listener;
 //This event track the players job level progression and ranks them up if they reach a certain level
 public class RankUpCheck implements Listener {
 
-
     @EventHandler
-    public void onSkillLevelUp(McMMOPlayerLevelChangeEvent event) {
+    public void onSkillLevelUp(McMMOPlayerLevelUpEvent event) {
         Bukkit.broadcastMessage("SKILL LEVEL UP TEST 1");
         Player p = event.getPlayer().getPlayer();
         McMMOPlayer mcPlayer = UserManager.getPlayer(p);
