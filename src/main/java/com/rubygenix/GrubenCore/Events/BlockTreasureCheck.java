3f02c159a0d4e0621ba5068cc30b2cc689d7ba65
OverCrave <over.crave@rubygenix.com>
2022-01-22 16:57:09 +0100

    Update BlockTreasureCheck.java

--- src/main/java/com/rubygenix/GrubenCore/Events/BlockTreasureCheck.java
+++ src/main/java/com/rubygenix/GrubenCore/Events/BlockTreasureCheck.java
@@ -43,80 +43,78 @@ public class BlockTreasureCheck implements Listener {
 
         OfflinePlayer oP = Main.I.server.getOfflinePlayer(p.getUniqueId());
 
-        if(p.getLocation().getBlockY() < 10){
-            if(Main.I.perms.has(p, "großmeister")){
-                int gain = getRandomNumber(treasureData.grossmeisterTokenMin, treasureData.grossmeisterTokenMax);
-                if(isCommonResource(m)){
-                    if(commonChance < treasureData.grossmeisterTokenChance*treasureData.commonRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
-                }
-                else if(isRareResource(m)){
-                    if(rareChance < treasureData.grossmeisterTokenChance*treasureData.rareRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+        if(Main.I.perms.has(p, "großmeister")){
+            int gain = getRandomNumber(treasureData.grossmeisterTokenMin, treasureData.grossmeisterTokenMax);
+            if(isCommonResource(m)){
+                if(commonChance < treasureData.grossmeisterTokenChance*treasureData.commonRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
             }
-            else if(Main.I.perms.has(p, "meister")){
-                int gain = getRandomNumber(treasureData.meisterTokenMin, treasureData.meisterTokenMax);
-                if(isCommonResource(m)){
-                    if(commonChance < treasureData.meisterTokenChance*treasureData.commonRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
-                }
-                else if(isRareResource(m)){
-                    if(rareChance < treasureData.meisterTokenChance*treasureData.rareRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+            else if(isRareResource(m)){
+                if(rareChance < treasureData.grossmeisterTokenChance*treasureData.rareRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
             }
-            else if(Main.I.perms.has(p, "geselle")){
-                int gain = getRandomNumber(treasureData.geselleTokenMin, treasureData.geselleTokenMax);
-                if(isCommonResource(m)){
-                    if(commonChance < treasureData.geselleTokenChance*treasureData.commonRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
-                }
-                else if(isRareResource(m)){
-                    if(rareChance < treasureData.geselleTokenChance*treasureData.rareRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+        }
+        else if(Main.I.perms.has(p, "meister")){
+            int gain = getRandomNumber(treasureData.meisterTokenMin, treasureData.meisterTokenMax);
+            if(isCommonResource(m)){
+                if(commonChance < treasureData.meisterTokenChance*treasureData.commonRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
             }
-            else if(Main.I.perms.has(p, "lehrling")){
-                int gain = getRandomNumber(treasureData.lehrlingTokenMin, treasureData.lehrlingTokenMax);
-                if(isCommonResource(m)){
-                    if(commonChance < treasureData.lehrlingTokenChance*treasureData.commonRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
-                }
-                else if(isRareResource(m)){
-                    if(rareChance < treasureData.lehrlingTokenChance*treasureData.rareRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+            else if(isRareResource(m)){
+                if(rareChance < treasureData.meisterTokenChance*treasureData.rareRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
             }
-            else{
-                int gain = getRandomNumber(treasureData.standardTokenMin, treasureData.standardTokenMax);
-                if(isCommonResource(m)){
-                    if(commonChance < treasureData.standardTokenChance*treasureData.commonRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+        }
+        else if(Main.I.perms.has(p, "geselle")){
+            int gain = getRandomNumber(treasureData.geselleTokenMin, treasureData.geselleTokenMax);
+            if(isCommonResource(m)){
+                if(commonChance < treasureData.geselleTokenChance*treasureData.commonRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
-                else if(isRareResource(m)){
-                    if(rareChance < treasureData.standardTokenChance*treasureData.rareRange){
-                        Main.I.econ.depositPlayer(oP, gain);
-                        p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
-                    }
+            }
+            else if(isRareResource(m)){
+                if(rareChance < treasureData.geselleTokenChance*treasureData.rareRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
+                }
+            }
+        }
+        else if(Main.I.perms.has(p, "lehrling")){
+            int gain = getRandomNumber(treasureData.lehrlingTokenMin, treasureData.lehrlingTokenMax);
+            if(isCommonResource(m)){
+                if(commonChance < treasureData.lehrlingTokenChance*treasureData.commonRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
+                }
+            }
+            else if(isRareResource(m)){
+                if(rareChance < treasureData.lehrlingTokenChance*treasureData.rareRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
+                }
+            }
+        }
+        else{
+            int gain = getRandomNumber(treasureData.standardTokenMin, treasureData.standardTokenMax);
+            if(isCommonResource(m)){
+                if(commonChance < treasureData.standardTokenChance*treasureData.commonRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
+                }
+            }
+            else if(isRareResource(m)){
+                if(rareChance < treasureData.standardTokenChance*treasureData.rareRange){
+                    Main.I.econ.depositPlayer(oP, gain);
+                    p.sendMessage(ChatColor.GREEN + "Du hast in diesem Block " + ChatColor.GOLD + gain + " Tokens" + ChatColor.GREEN + " gefunden!");
                 }
             }
         }
@@ -129,10 +127,12 @@ public class BlockTreasureCheck implements Listener {
     }
 
     private Boolean isCommonResource(Material m){
-        return (m == Material.DEEPSLATE || m == Material.DEEPSLATE_COAL_ORE || m == Material.DEEPSLATE_IRON_ORE || m == Material.DEEPSLATE_COPPER_ORE || m == Material.DEEPSLATE_LAPIS_ORE || m == Material.DEEPSLATE_REDSTONE_ORE);
+        return (m == Material.DEEPSLATE || m == Material.DEEPSLATE_COAL_ORE || m == Material.DEEPSLATE_IRON_ORE || m == Material.DEEPSLATE_COPPER_ORE || m == Material.DEEPSLATE_LAPIS_ORE || m == Material.DEEPSLATE_REDSTONE_ORE
+                || m == Material.STONE || m == Material.COAL_ORE || m == Material.IRON_ORE || m == Material.COPPER_ORE || m == Material.LAPIS_ORE || m == Material.REDSTONE_ORE);
     }
 
     private Boolean isRareResource(Material m){
-        return (m == Material.DEEPSLATE_DIAMOND_ORE || m == Material.DEEPSLATE_GOLD_ORE || m == Material.DEEPSLATE_EMERALD_ORE || m == Material.ANCIENT_DEBRIS);
+        return (m == Material.DEEPSLATE_DIAMOND_ORE || m == Material.DEEPSLATE_GOLD_ORE || m == Material.DEEPSLATE_EMERALD_ORE
+                || m == Material.ANCIENT_DEBRIS || m == Material.DIAMOND_ORE || m == Material.GOLD_ORE || m == Material.EMERALD_ORE);
     }
 }
